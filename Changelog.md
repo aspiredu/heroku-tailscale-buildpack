5c36c8d350eab5c1cb1e9920d440ac1bb8e7f984
Tim Schilling <tim@aspiredu.com>
2023-05-10 16:40:06 -0500

    Only start Tailscale when the auth key is present

--- Changelog.md
+++ Changelog.md
@@ -4,3 +4,5 @@
   https://github.com/heroku/heroku-buildpack-pgbouncer and
   tailscale-docker and tailscale-heroku.
 * Move the process to start tailscale into the .profile.d/ script.
+* Only start Tailscale when the auth key is present in the environment 
+  variables.
--- bin/heroku-tailscale-start.sh
+++ bin/heroku-tailscale-start.sh
@@ -10,20 +10,25 @@ function indent() {
   sed -e 's/^/       /'
 }
 
-log "Starting Tailscale"
+if [ -z "$TAILSCALE_AUTH_KEY" ]; then
+  log "Skipping Tailscale"
 
-tailscaled -verbose ${TAILSCALED_VERBOSE:-0} --tun=userspace-networking --socks5-server=localhost:1055 &
-until tailscale up \
-  --authkey=${TAILSCALE_AUTH_KEY} \
-  --hostname=${TAILSCALE_HOSTNAME:-$(hostname)} \
-  --accept-dns=${TAILSCALE_ACCEPT_DNS:-true} \
-  --accept-routes=${TAILSCALE_ACCEPT_ROUTES:-true} \
-  --advertise-exit-node=${TAILSCALE_ADVERTISE_EXIT_NODE:-false} \
-  --shields-up=${TAILSCALE_SHIELDS_UP:-false}
-do
-  log "Waiting for 5s for Tailscale to start"
-  sleep 5
-done
+else
+  log "Starting Tailscale"
 
-export ALL_PROXY=socks5://localhost:1055/
-log "Tailscale started"
+  tailscaled -verbose ${TAILSCALED_VERBOSE:-0} --tun=userspace-networking --socks5-server=localhost:1055 &
+  until tailscale up \
+    --authkey=${TAILSCALE_AUTH_KEY} \
+    --hostname=${TAILSCALE_HOSTNAME:-$(hostname)} \
+    --accept-dns=${TAILSCALE_ACCEPT_DNS:-true} \
+    --accept-routes=${TAILSCALE_ACCEPT_ROUTES:-true} \
+    --advertise-exit-node=${TAILSCALE_ADVERTISE_EXIT_NODE:-false} \
+    --shields-up=${TAILSCALE_SHIELDS_UP:-false}
+  do
+    log "Waiting for 5s for Tailscale to start"
+    sleep 5
+  done
+
+  export ALL_PROXY=socks5://localhost:1055/
+  log "Tailscale started"
+fi
